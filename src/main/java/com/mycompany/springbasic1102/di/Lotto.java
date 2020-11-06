d5def7d4ccff745b76d00cca09431a368c75c050
teacher <teacher@192.168.1.180>
2020-11-06 20:57:38 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/di/Lotto.java
@@ -0,0 +1,58 @@
+package com.mycompany.springbasic1102.di;
+
+import java.util.Set;
+
+public class Lotto {
+    private Integer amount;
+    private Integer begin;
+    private Integer end;
+    private Set<Integer> nums;
+
+    public Lotto() {
+    }
+
+    public Lotto(Integer amount, Integer begin, Integer end) {
+        this.amount = amount;
+        this.begin = begin;
+        this.end = end;
+        
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getBegin() {
+        return begin;
+    }
+
+    public void setBegin(Integer begin) {
+        this.begin = begin;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+    public void setEnd(Integer end) {
+        this.end = end;
+    }
+
+    public Set<Integer> getNums() {
+        return nums;
+    }
+
+    public void setNums(Set<Integer> nums) {
+        this.nums = nums;
+    }
+
+    @Override
+    public String toString() {
+        return "Lotto{" + "amount=" + amount + ", begin=" + begin + ", end=" + end + ", nums=" + nums + '}';
+    }
+    
+}
