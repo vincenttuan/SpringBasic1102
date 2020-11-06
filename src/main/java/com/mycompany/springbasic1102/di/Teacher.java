8892c8770e7d06defc8b44411f554caa80cd028b
teacher <teacher@192.168.1.180>
2020-11-06 19:50:38 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/di/Teacher.java
+++ src/main/java/com/mycompany/springbasic1102/di/Teacher.java
@@ -1,8 +1,11 @@
 package com.mycompany.springbasic1102.di;
 
+import java.util.List;
+
 public class Teacher {
     private String name;
-
+    private List<String> cls; // 班級
+    
     public Teacher() {
     }
 
@@ -18,9 +21,19 @@ public class Teacher {
         this.name = name;
     }
 
+    public List<String> getCls() {
+        return cls;
+    }
+
+    public void setCls(List<String> cls) {
+        this.cls = cls;
+    }
+
     @Override
     public String toString() {
-        return "Teacher{" + "name=" + name + '}';
+        return "Teacher{" + "name=" + name + ", cls=" + cls + '}';
     }
+
+    
     
 }
