58d62e0b48b2c77684902e3cd419faf4c59fc17b
teacher <teacher@192.168.1.180>
2020-11-06 20:49:53 +0800

    SSH

--- src/test/java/di/Test_Student2.java
+++ src/test/java/di/Test_Student2.java
@@ -29,6 +29,8 @@ public class Test_Student2 {
         System.out.println(s9);
         Teacher t3 = ctx.getBean("t3", Teacher.class);
         System.out.println(t3);
+        Teacher t4 = ctx.getBean("t4", Teacher.class);
+        System.out.println(t4);
         ctx.close();
     }
 }
