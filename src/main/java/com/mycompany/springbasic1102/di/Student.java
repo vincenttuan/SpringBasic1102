e3f9980345bbcc88686c20736826b749629f0926
teacher <teacher@192.168.1.180>
2020-11-06 20:05:20 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/di/Student.java
+++ src/main/java/com/mycompany/springbasic1102/di/Student.java
@@ -47,7 +47,7 @@ public class Student {
 
     @Override
     public String toString() {
-        return "Student{" + "name=" + name + ", age=" + age + ", teacher=" + teacher.getName() + '}';
+        return "Student{" + "name=" + name + ", age=" + age + ", teacher=" + (teacher == null ? "" : teacher.getName()) + '}';
     }
     
     
