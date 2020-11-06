a2105ac71f5a85c814c9138908ee8c28f58427fb
teacher <teacher@192.168.1.180>
2020-11-06 18:52:22 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/di/Student.java
+++ src/main/java/com/mycompany/springbasic1102/di/Student.java
@@ -4,6 +4,14 @@ public class Student {
     private String name;
     private int age;
 
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
     public String getName() {
         return name;
     }
