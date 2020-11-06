67a9fc2d90cbf7b8be6f5836495ee79351a156fe
teacher <teacher@192.168.1.180>
2020-11-06 21:06:18 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/di/LottoFactory.java
@@ -0,0 +1,24 @@
+package com.mycompany.springbasic1102.di;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class LottoFactory implements FactoryBean<Lotto>{
+
+    @Override
+    public Lotto getObject() throws Exception {
+        return new Lotto(5, 1, 39);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Lotto.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    
+    
+}
