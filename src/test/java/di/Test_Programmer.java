4aaafa861d05f3274cf1c40de6aee744dc74b6f6
teacher <teacher@192.168.1.180>
2020-11-09 19:12:29 +0800

    SSH

---
+++ src/test/java/di/Test_Programmer.java
@@ -0,0 +1,17 @@
+package di;
+
+import com.mycompany.springbasic1102.di.byname.OracleProgrammer;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test_Programmer {
+    @Test
+    public void t1() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config-2.xml");
+        // byName
+        OracleProgrammer op = ctx.getBean("oracleProgrammer", OracleProgrammer.class);
+        System.out.println(op);
+        ctx.close();
+    }
+}
