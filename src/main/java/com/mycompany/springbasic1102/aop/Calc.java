91d3109c786831500ac569253b8f5c75d6eb56f4
teacher <teacher@192.168.1.180>
2020-11-13 20:37:15 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/aop/Calc.java
@@ -0,0 +1,8 @@
+package com.mycompany.springbasic1102.aop;
+
+public interface Calc {
+    int add(int x, int y);
+    int sub(int x, int y);
+    int mul(int x, int y);
+    int div(int x, int y);
+}
---
+++ src/main/java/com/mycompany/springbasic1102/aop/CalcImpl.java
@@ -0,0 +1,25 @@
+package com.mycompany.springbasic1102.aop;
+
+public class CalcImpl implements Calc {
+
+    @Override
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    @Override
+    public int sub(int x, int y) {
+        return x - y;
+    }
+
+    @Override
+    public int mul(int x, int y) {
+        return x * y;
+    }
+
+    @Override
+    public int div(int x, int y) {
+        return x / y;
+    }
+    
+}
