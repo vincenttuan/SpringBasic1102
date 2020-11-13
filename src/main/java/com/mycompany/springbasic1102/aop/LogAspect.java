020aa583bedca0edaffb5b8f519ca594664be1d2
teacher <teacher@192.168.1.180>
2020-11-13 20:51:55 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
@@ -0,0 +1,10 @@
+package com.mycompany.springbasic1102.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspect {
+    
+}
