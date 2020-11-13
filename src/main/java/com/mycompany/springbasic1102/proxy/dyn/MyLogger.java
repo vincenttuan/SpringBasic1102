e12cb20d55627ef7b4962ab0249f7a7b65bcd557
teacher <teacher@192.168.1.180>
2020-11-13 19:44:04 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/proxy/dyn/MyLogger.java
@@ -0,0 +1,12 @@
+package com.mycompany.springbasic1102.proxy.dyn;
+
+import java.util.Arrays;
+
+public class MyLogger {
+    public static void before(Class cls, String methodName, Object[] args) {
+        System.out.printf("before log: %s %s %s", cls, methodName, Arrays.toString(args));
+    }
+    public static void end(Class cls, String methodName, Object result) {
+        System.out.printf("end log: %s %s %s", cls, methodName, result);
+    }
+}
