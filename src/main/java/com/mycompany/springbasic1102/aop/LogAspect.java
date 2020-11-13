cef5030ed3fe929085b4bf1baea4f2cad1066b5b
teacher <teacher@192.168.1.180>
2020-11-13 21:17:55 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
+++ src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
@@ -1,5 +1,6 @@
 package com.mycompany.springbasic1102.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -20,7 +21,16 @@ public class LogAspect {
     //@Before(value = "execution(* com.mycompany.springbasic1102.aop.*.*(..))") // 切入點表達式
     //@Before(value = "execution(* *.*(..))") // 切入點表達式
     @Before(value = "cut()") // 切入點參照
-    public void before() {
+    public void before() { 
         System.out.println("前置通知:");
     }
+    
+    // 後置通知(Advice)
+    // 無論是否有例外發生都會被執行, 因後置通知作用在 finally 中...
+    @After(value = "cut()") // 切入點參照
+    public void after() {
+        System.out.println("後置通知:");
+    }
+    
+    
 }
--- src/test/java/aop/Test1.java
+++ src/test/java/aop/Test1.java
@@ -9,8 +9,9 @@ public class Test1 {
     public void t1() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("aop-config.xml");
         Calc calc = ctx.getBean("calcImpl", Calc.class);
-        System.out.println(calc.add(2, 3));
-        System.out.println(calc.mul(2, 3));
+        //System.out.println(calc.add(2, 3));
+        //System.out.println(calc.mul(2, 3));
+        System.out.println(calc.div(4, 0));
         ctx.close();
     }
 }
