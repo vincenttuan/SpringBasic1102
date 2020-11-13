8d91344d662352e8281dc37f0af1afb80fdb8afd
teacher <teacher@192.168.1.180>
2020-11-13 20:58:18 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
+++ src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
@@ -1,10 +1,18 @@
 package com.mycompany.springbasic1102.aop;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Order(1) // 數字越小越先被執行
 public class LogAspect {
     
+    // 前置通知
+    @Before(value = "execution(public int com.mycompany.springbasic1102.aop.CalcImpl.add(int, int))") // 切入點表達式
+    public void before() {
+        System.out.println("前置通知:");
+    }
 }
