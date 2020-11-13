05e772718433877eb8b5fa4c6a5c0fdc148db7aa
teacher <teacher@192.168.1.180>
2020-11-13 21:12:49 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
+++ src/main/java/com/mycompany/springbasic1102/aop/LogAspect.java
@@ -2,6 +2,7 @@ package com.mycompany.springbasic1102.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +10,16 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Order(1) // 數字越小越先被執行
 public class LogAspect {
-    
+    @Pointcut(value = "execution(* com.mycompany.springbasic1102.aop.*.*(..))")
+    public void cut() {
+    }
     // 前置通知(Advice)
-    @Before(value = "execution(public int com.mycompany.springbasic1102.aop.CalcImpl.add(int, int))") // 切入點表達式
+    //@Before(value = "execution(public int com.mycompany.springbasic1102.aop.CalcImpl.add(int, int))") // 切入點表達式
+    //@Before(value = "execution(public int com.mycompany.springbasic1102.aop.CalcImpl.*(int, int))") // 切入點表達式
+    //@Before(value = "execution(public int com.mycompany.springbasic1102.aop.*.*(int, int))") // 切入點表達式
+    //@Before(value = "execution(* com.mycompany.springbasic1102.aop.*.*(..))") // 切入點表達式
+    //@Before(value = "execution(* *.*(..))") // 切入點表達式
+    @Before(value = "cut()") // 切入點參照
     public void before() {
         System.out.println("前置通知:");
     }
--- src/test/java/aop/Test1.java
+++ src/test/java/aop/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("aop-config.xml");
         Calc calc = ctx.getBean("calcImpl", Calc.class);
         System.out.println(calc.add(2, 3));
+        System.out.println(calc.mul(2, 3));
         ctx.close();
     }
 }
