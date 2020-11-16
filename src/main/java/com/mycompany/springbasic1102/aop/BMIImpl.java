62838516a260b98cdd2712efd1d7a018126cda42
teacher <teacher@192.168.1.180>
2020-11-16 21:31:23 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/aop/BMIImpl.java
+++ src/main/java/com/mycompany/springbasic1102/aop/BMIImpl.java
@@ -7,6 +7,9 @@ public class BMIImpl implements BMI {
 
     @Override
     public Double calc(Double h, Double w) {
+        if(w <= 0 || h <= 0) {
+            throw new RuntimeException("身高體重資料不正確");
+        }
         return w / Math.pow(h/100, 2);
     }
     
