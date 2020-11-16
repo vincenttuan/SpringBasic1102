24dba2be71bd71decdba2a4f0cb701b4fd8f45eb
teacher <teacher@192.168.1.180>
2020-11-16 19:48:11 +0800

    SSH

--- src/test/java/jdbc/Test1.java
+++ src/test/java/jdbc/Test1.java
@@ -5,11 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Test1 {
-    
+    ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+    JdbcTemplate jdbcTemplate = ctx.getBean("jdbcTemplate", JdbcTemplate.class);
+        
     @Test
     public void t1() {
-        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
-        JdbcTemplate jdbcTemplate = ctx.getBean("jdbcTemplate", JdbcTemplate.class);
         System.out.println(jdbcTemplate);
+        create();
+    }
+    // CRUD - Create 新增資料
+    private void create() {
+        String sql = "Insert Into Emp(ename, age, sex) Values('John', 18, '男')";
+        jdbcTemplate.update(sql);
     }
 }
