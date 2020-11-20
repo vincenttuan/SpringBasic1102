e3276dad2c7620a734e48a795bd12173f309d158
teacher <teacher@192.168.1.180>
2020-11-20 19:43:17 +0800

    SSH

---
+++ src/test/java/jdbc/tx/Tx1.java
@@ -0,0 +1,25 @@
+package jdbc.tx;
+
+import com.mycompany.springbasic1102.jdbc.tx.BookDao;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Tx1 {
+    @Test
+    public void t1() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+        BookDao bookDao = ctx.getBean("bookDaoImpl", BookDao.class);
+        System.out.println(bookDao);
+        // wid = 1 顧客的錢包編號
+        int wid = 1;
+        // bid = 1 書籍的編號
+        int bid = 1;
+        //  查詢書籍價格
+        int price = bookDao.getPrice(bid);
+        // 進行交易
+        System.out.println("交易開始");
+        bookDao.updateStock(bid);
+        bookDao.updateWallet(wid, price);
+        System.out.println("交易完成");
+    }
+}
