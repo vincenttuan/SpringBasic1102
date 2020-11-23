84f46c35365b295be2c87fd3a6dbbc8f52958846
teacher <teacher@192.168.1.180>
2020-11-23 20:59:56 +0800

    SSH

---
+++ src/test/java/jdbc/invoice/Test3.java
@@ -0,0 +1,26 @@
+package jdbc.invoice;
+
+import com.mycompany.springbasic1102.jdbc.invoice.dao.InvoiceDao;
+import com.mycompany.springbasic1102.jdbc.invoice.entity.Invoice;
+import com.mycompany.springbasic1102.jdbc.invoice.entity.Item;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.simpleflatmapper.jdbc.spring.JdbcTemplateMapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:jdbc-config.xml"})
+public class Test3 {
+    @Autowired
+    private InvoiceDao invoiceDao;
+    @Test
+    public void t3() {
+        System.out.println(invoiceDao.queryIntem());
+    }
+}
