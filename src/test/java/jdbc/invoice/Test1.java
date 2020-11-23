071e9fd983b9a8c56b228f5e902b6a5b10e45d60
teacher <teacher@192.168.1.180>
2020-11-23 19:36:36 +0800

    SSH

--- src/test/java/jdbc/invoice/Test1.java
+++ src/test/java/jdbc/invoice/Test1.java
@@ -1,5 +1,6 @@
 package jdbc.invoice;
 
+import com.mycompany.springbasic1102.jdbc.invoice.entity.Invoice;
 import com.mycompany.springbasic1102.jdbc.invoice.entity.Item;
 import com.mycompany.springbasic1102.jdbc.invoice.entity.ItemProduct;
 import java.sql.ResultSet;
@@ -19,14 +20,20 @@ public class Test1 {
         @Override
         public Item mapRow(ResultSet rs, int i) throws SQLException {
             Item item = new Item();
-            item.setId(rs.getInt("id"));
-            item.setAmount(rs.getInt("amount"));
+            item.setId(rs.getInt("id")); // 注入 id
+            item.setAmount(rs.getInt("amount"));  // 注入 amount
             Integer ipid = rs.getInt("ipid");
+            // 注入 product 物件
             String sql_1 = "SELECT * FROM ITEMPRODUCT WHERE id = ?";
             ItemProduct product = jdbcTemplate.queryForObject(
                     sql_1, new BeanPropertyRowMapper<>(ItemProduct.class), ipid);
-            item.setProduct(product);
+            item.setProduct(product); //  // 注入 product 物件
+            // 注入 invoice 物件
             Integer invid = rs.getInt("invid");
+            String sql_2 = "SELECT * FROM INVOICE WHERE id = ?";
+            Invoice invoice = jdbcTemplate.queryForObject(
+                    sql_2, new BeanPropertyRowMapper<>(Invoice.class), invid);
+            item.setInvoice(invoice); // 注入 invoice 物件
             return item;
         }
     }
