9144272a2f989e42cd6a8035f51503f248c7f094
teacher <teacher@192.168.1.180>
2020-11-27 21:43:29 +0800

    SSH

---
+++ src/test/java/jpa/single/Delete.java
@@ -0,0 +1,19 @@
+package jpa.single;
+
+import com.mycompany.springbasic1102.jpa.entities.Customer;
+import jpa.JPATemplate;
+import org.junit.Test;
+
+public class Delete extends JPATemplate {
+    @Test
+    public void t1() {
+        //Customer c1 = (Customer)session.get(Customer.class, 4);
+        //session.delete(c1);
+        
+        Customer c2 = new Customer();
+        c2.setId(5);
+        c2.setLastName("aaa");
+        session.delete(c2);
+        
+    }
+}
