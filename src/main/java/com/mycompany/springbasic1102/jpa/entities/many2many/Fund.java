9e259a96ea3e4d4ea8962679d31fe76e8dbf53bd
teacher <teacher@192.168.1.180>
2020-12-04 19:55:52 +0800

    SSH

--- src/main/java/com/mycompany/springbasic1102/jpa/entities/many2many/Fund.java
+++ src/main/java/com/mycompany/springbasic1102/jpa/entities/many2many/Fund.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.springbasic1102.jpa.entities.many2many;
 
-/**
- *
- * @author teacher
- */
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Fund")
 public class Fund {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private String name;
+    
+    @JoinTable(
+            name = "Fund_Cstock",
+            joinColumns = {@JoinColumn(name = "fund_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "cstock_id", referencedColumnName = "id")}
+    )
+    @ManyToMany
+    private Set<Cstock> cstocks = new LinkedHashSet<>();
     
 }
