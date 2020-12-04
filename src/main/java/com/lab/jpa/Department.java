b48ac039ac3ab94a4e164d91581c5a591fbe0604
teacher <teacher@192.168.1.180>
2020-12-04 21:23:24 +0800

    SSH

--- src/main/java/com/lab/jpa/Department.java
+++ src/main/java/com/lab/jpa/Department.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.lab.jpa;
 
-/**
- *
- * @author teacher
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
 public class Department {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     
 }
