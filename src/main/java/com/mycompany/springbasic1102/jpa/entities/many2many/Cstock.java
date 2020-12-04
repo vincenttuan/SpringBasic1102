ec097b9e70172d30e287b0c4b5cd3ce8486982db
teacher <teacher@192.168.1.180>
2020-12-04 19:49:34 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/jpa/entities/many2many/Cstock.java
@@ -0,0 +1,50 @@
+package com.mycompany.springbasic1102.jpa.entities.many2many;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cstock")
+public class Cstock {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column
+    private String symbol;
+    
+    @ManyToMany
+    private Set<Fund> funds = new LinkedHashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public Set<Fund> getFunds() {
+        return funds;
+    }
+
+    public void setFunds(Set<Fund> funds) {
+        this.funds = funds;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/springbasic1102/jpa/entities/many2many/Fund.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.springbasic1102.jpa.entities.many2many;
+
+/**
+ *
+ * @author teacher
+ */
+public class Fund {
+    
+}
